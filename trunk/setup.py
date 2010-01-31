1c8d0c22e47c84f9ddb9b484edf7ec759bce7daf
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-01-31 14:31:21 +0000

    Added standard setup.py

---
+++ trunk/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='Marave',
+      version='0.1',
+      description='A relaxed text editor',
+      author='Roberto Alsina',
+      author_email='ralsina@netmanagers.com.ar',
+      url='http://marave.googlecode.com',
+      packages=['marave'],
+      scripts=['marave/marave'],
+      package_data={'marave': ['backgrounds/*jpg',
+                               'icons/*svg',
+                               'clicks/*wav',
+                               'radios.txt'
+                               ]},
+     )

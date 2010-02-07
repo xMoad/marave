994cc6c41fbca5922d598472516ecd783c1558f5
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-07 19:21:26 +0000

    Creating branch for 0.2

---
+++ branches/0.2/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='Marave',
+      version='0.2',
+      description='A relaxed text editor',
+      author='Roberto Alsina',
+      author_email='ralsina@netmanagers.com.ar',
+      url='http://marave.googlecode.com',
+      packages=['marave'],
+      scripts=['marave/marave'],
+      package_data={'marave': ['backgrounds/*jpg',
+                               'icons/*svg',
+                               'clicks/*wav',
+                               'themes/*',
+                               'radios.txt'
+                               ]},
+     )

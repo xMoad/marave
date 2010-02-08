414daf191c9b71608a0f5e6bd18887bcc9c1d7bf
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-08 02:17:29 +0000

    Move stylesheets to a folder, add UI to handle them (not functional)

--- trunk/setup.py
+++ trunk/setup.py
@@ -14,6 +14,7 @@ setup(name='Marave',
                                'icons/*svg',
                                'clicks/*wav',
                                'themes/*',
+                               'stylesheets/*',
                                'radios.txt'
                                ]},
      )

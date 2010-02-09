2fefefb0423f86c8fbf205a27a6e73591fcbdde1
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-09 19:42:16 +0000

    Preparing 0.4.1

---
+++ branches/0.4.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='Marave',
+      version='0.5',
+      description='A relaxed text editor',
+      author='Roberto Alsina',
+      author_email='ralsina@netmanagers.com.ar',
+      url='http://marave.googlecode.com',
+      packages=['marave'],
+      scripts=['marave/marave'],
+      package_data={'marave': ['backgrounds/*jpg',
+                               'icons/*svg',
+                               'clicks/*wav',
+                               'themes/*',
+                               'themes/*',
+                               'stylesheets/*',
+                               'radios.txt'
+                               ]},
+     )

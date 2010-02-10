0db36f6e73019fa153f047a2e69023c32ba7c2c2
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-10 15:39:08 +0000

    Branch to fix Issue 15!

---
+++ branches/bdgraue/marave/spelltextedit.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+__license__ = 'MIT'
+__copyright__ = '2009, John Schember <john@nachtimwald.com>'
+__docformat__ = 'restructuredtext en'
+ 
+import re
+import sys
+import codecs
+
+try:
+    import enchant
+except ImportError:
+    enchant = None
+
+
+from PyQt4.Qt import QAction
+from PyQt4.Qt import QApplication
+from PyQt4.Qt import QEvent
+from PyQt4.Qt import QMenu
+from PyQt4.Qt import QMouseEvent
+from PyQt4.Qt import QPlainTextEdit
+from PyQt4.Qt import QSyntaxHighlighter
+from PyQt4.Qt import QTextCharFormat
+from PyQt4.Qt import QTextCursor
+from PyQt4.Qt import Qt
+from PyQt4.Qt import QColor
+from PyQt4.Qt import QPalette
+from PyQt4.QtCore import pyqtSignal
+from PyQt4 import QtGui, QtCore
+ 
+class SpellTextEdit(QPlainTextEdit):
+ 
+    def __init__(self, *args):
+        QPlainTextEdit.__init__(self, *args)
+        self.initDict()
+
+    def initDict(self, lang=None):
+        if not enchant:
+            return
+        # Default dictionary based on the current locale.
+        if lang==None:
+            self.dict = enchant.Dict()
+        else:
+            self.dict = enchant.Dict(lang)
+        self.highlighter = Highlighter(self.document())
+        self.highlighter.setDict(self.dict)
+        self.highlighter.rehighlight()
+
+    def killDict(self):
+        print 'Disabling spellchecker'
+        self.highlighter.setDocument(None)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            # Rewrite the mouse event to a left button event so the cursor is
+            # moved to the location of the pointer.
+            event = QMouseEvent(QEvent.MouseButtonPress, event.pos(),
+                Qt.LeftButton, Qt.LeftButton, Qt.NoModifier)
+        QPlainTextEdit.mousePressEvent(self, event)
+ 
+    def contextMenuEvent(self, event):
+        popup_menu = self.createStandardContextMenu()
+        pal=QApplication.instance().palette()
+        # This fixes Issue 20
+        popup_menu.setStyleSheet("""
+                                  * { background-color: %s;
+                                      color: %s;}
+                                  """%(unicode(pal.color(QPalette.Button).name()),
+                                        unicode(pal.color(QPalette.WindowText).name())))
+ 
+        # Select the word under the cursor.
+        cursor = self.textCursor()
+        cursor.select(QTextCursor.WordUnderCursor)
+        self.setTextCursor(cursor)
+ 
+        # Check if the selected word is misspelled and offer spelling
+        # suggestions if it is.
+        if enchant:
+            if self.textCursor().hasSelection():
+                text = unicode(self.textCursor().selectedText())
+                if not self.dict.check(text):
+                    spell_menu = QMenu('Spelling Suggestions')
+                    for word in self.dict.suggest(text):
+                        action = SpellAction(word, spell_menu)
+                        action.correct.connect(self.correctWord)
+                        spell_menu.addAction(action)
+                    # Only add the spelling suggests to the menu if there are
+                    # suggestions.
+                    if len(spell_menu.actions()) != 0:
+                        popup_menu.insertSeparator(popup_menu.actions()[0])
+                        popup_menu.insertMenu(popup_menu.actions()[0], spell_menu)
+                        
+        # FIXME: add change dict and disable spellcheck options
+ 
+        popup_menu.exec_(event.globalPos())
+ 
+    def correctWord(self, word):
+        '''
+        Replaces the selected text with word.
+        '''
+        cursor = self.textCursor()
+        cursor.beginEditBlock()
+ 
+        cursor.removeSelectedText()
+        cursor.insertText(word)
+ 
+        cursor.endEditBlock()
+
+    def save(self):
+        if not self.docName:
+            self.saveas()
+        else:
+            f=codecs.open(self.docName,'w+','utf-8')
+            f.truncate()
+            f.write(unicode(self.toPlainText()))
+            f.close()
+            self.document().setModified(False)
+
+    def saveas(self):
+        QtCore.QCoreApplication.instance().setOverrideCursor(QtCore.Qt.ArrowCursor)
+        fname=unicode(QtGui.QFileDialog.getSaveFileName(self.parent()))
+        QtCore.QCoreApplication.instance().restoreOverrideCursor()
+        if fname:
+            self.docName=fname
+            self.save()
+
+    def new(self):
+        QtCore.QCoreApplication.instance().setOverrideCursor(QtCore.Qt.ArrowCursor)
+        try:
+            if self.document().isModified():
+                r=QtGui.QMessageBox.question(self.parent(), "New Document - Marave", "The document \"%s\" has been modified."\
+                    "\nDo you want to save your changes or discard them?"%self.docName or "UNNAMED",
+                    QtGui.QMessageBox.Save|QtGui.QMessageBox.Discard|QtGui.QMessageBox.Cancel,QtGui.QMessageBox.Cancel)
+                if r==QtGui.QMessageBox.Save:
+                    self.save()
+                elif r==QtGui.QMessageBox.Discard:
+                    self.docName=''
+                    self.setPlainText('')
+                    self.parent().setWindowTitle('Untitled - Marave')
+            else:
+                    self.docName=''
+                    self.setPlainText('')
+                    self.parent().setWindowTitle('Untitled - Marave')
+        except:
+            pass
+        QtCore.QCoreApplication.instance().restoreOverrideCursor()
+
+    def open(self, fname=None):
+        self.new()
+        if self.docName:
+            return
+        if not fname:
+            QtCore.QCoreApplication.instance().setOverrideCursor(QtCore.Qt.ArrowCursor)
+            fname=unicode(QtGui.QFileDialog.getOpenFileName(self.parent()))
+            QtCore.QCoreApplication.instance().restoreOverrideCursor()
+        if fname:
+            self.docName=fname
+            self.setPlainText(codecs.open(fname,'r','utf-8').read())
+            
+        self.parent().setWindowTitle('%s - Marave'%self.docName)
+
+    def smaller(self):
+        f=self.font()
+        f.setPointSize(f.pointSize()-1)
+        self.setFont(f)
+        self.parent().settings.setValue('fontsize',self.font().pointSize())
+        self.parent().settings.sync()
+        
+    def larger(self):
+        f=self.font()
+        f.setPointSize(f.pointSize()+1)
+        self.setFont(f)
+        self.parent().settings.setValue('fontsize',self.font().pointSize())
+        self.parent().settings.sync()
+
+    def default(self):
+        f=self.font()
+        f.setPointSize(self.defSize)
+        self.setFont(f)
+        self.parent().settings.setValue('fontsize',self.font().pointSize())
+        self.parent().settings.sync()
+
+    def mouseMoveEvent(self, ev):
+        self.parent().showButtons()
+        self.parent().showCursor()
+        return QtGui.QPlainTextEdit.mouseMoveEvent(self, ev)
+ 
+ 
+class Highlighter(QSyntaxHighlighter):
+ 
+    WORDS = u'(?iu)[\w\']+'
+ 
+    def __init__(self, *args):
+        QSyntaxHighlighter.__init__(self, *args)
+ 
+        self.dict = None
+ 
+    def setDict(self, dict):
+        self.dict = dict
+ 
+    def highlightBlock(self, text):
+        if not self.dict:
+            return
+ 
+        text = unicode(text)
+ 
+        format = QTextCharFormat()
+        #format.setUnderlineColor(Qt.red)
+        format.setUnderlineStyle(QTextCharFormat.DotLine)
+ 
+        for word_object in re.finditer(self.WORDS, text):
+            if not self.dict.check(word_object.group()):
+                self.setFormat(word_object.start(),
+                    word_object.end() - word_object.start(), format)
+ 
+ 
+class SpellAction(QAction):
+ 
+    '''
+    A special QAction that returns the text in a signal.
+    '''
+ 
+    correct = pyqtSignal(unicode)
+ 
+    def __init__(self, *args):
+        QAction.__init__(self, *args)
+ 
+        self.triggered.connect(lambda x: self.correct.emit(
+            unicode(self.text())))
+ 
+ 
+def main(args=sys.argv):
+    app = QApplication(args)
+ 
+    spellEdit = SpellTextEdit()
+    spellEdit.show()
+ 
+    return app.exec_()
+ 
+if __name__ == '__main__':
+    sys.exit(main())

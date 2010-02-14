d0bd381b5035f34d6f9df196e740dbe0e5bac5bc
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-14 21:29:07 +0000

    he_IL, not he_IN dude\!

--- marave/marave_he.ts
+++ marave/marave_he.ts
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="he_IN">
+<!DOCTYPE TS><TS version="1.1" language="he_IL">
 <context>
     <name>Form</name>
     <message>

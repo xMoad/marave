bfe6b0bf3f310bceaf752f704dc3aea5e06ae7da
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-16 19:01:34 +0000

    Extend the wrapper until it's useful

--- trunk/marave/highlight/test-highlighter.py
+++ trunk/marave/highlight/test-highlighter.py
@@ -7,10 +7,32 @@ from SyntaxHighlighter import srchiliteqt
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
+    window=QtGui.QWidget()
+    window.show()
+        
     w = QtGui.QPlainTextEdit()
-    w.show()
+    langs=srchiliteqt.LanguageComboBox()
+    styles=srchiliteqt.StyleComboBox()
+    
+    
+    layout=QtGui.QVBoxLayout()
+    layout.addWidget(langs)
+    layout.addWidget(styles)
+    layout.addWidget(w)
+
+    window.setLayout(layout)
 
     hp=srchiliteqt.Qt4SyntaxHighlighter(w.document())
+
+    def changeLang(idx):
+        print langs.currentText(), styles.currentText()
+        hp.init(langs.currentText(), styles.currentText())
+        hp.setFormattingStyle(styles.currentText())
+        hp.rehighlight()
+
+    langs.currentIndexChanged.connect(changeLang)
+    styles.currentIndexChanged.connect(changeLang)
+    
     hp.init('python.lang')
 
     if len(sys.argv) >1:

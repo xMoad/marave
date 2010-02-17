c835868e9b4094eeaf3c6f17dcd06f7c249e40e3
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-17 04:45:08 +0000

    extended setup.py with a build_hl command to build the syntax highlighter, and documented it

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from distutils.cmd import Command
 from distutils.core import setup
+import os
+
+class build_hl(Command):
+    """Builds the syntax highlighter extension"""
+    user_options = []
+    description = "Build the syntax highlighter extension"
+    
+    def initialize_options(self):
+        self._dir = os.path.abspath(os.path.dirname(__file__))
+
+    def finalize_options(self):
+        pass    
+    
+    def run(self):
+        os.chdir(os.path.join(self._dir,'marave','highlight'))
+        r=os.system('python configure.py')
+        if r==0:
+            os.system('make')
+        os.chdir(self._dir)
 
 setup(name='Marave',
       version='0.6',
@@ -19,4 +39,5 @@ setup(name='Marave',
                                'highlight/*',
                                'radios.txt'
                                ]},
+     cmdclass = { 'build_hl': build_hl },
      )

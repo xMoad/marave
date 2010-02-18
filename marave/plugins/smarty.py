541d4f8cfb0e988c6d39b7e675c5ce35fa23616d
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-18 12:42:24 +0000

    First plugin that 'works'

--- marave/plugins/smarty.py
+++ marave/plugins/smarty.py
@@ -3,6 +3,8 @@
 from plugins import Plugin
 
 class Smarty(Plugin):
-    shortcut='Ctrl+Shift+\''
+    shortcut='Ctrl+\''
     description='Smart quote and dash replacement'
-    
+
+    def run(self):
+        print "my client window is:", self.client

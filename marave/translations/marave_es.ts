6439156c9db200327d43aacb37a1c3aeca256271
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-18 11:21:23 +0000

    Updated spanish translation

--- marave/translations/marave_es.ts
+++ marave/translations/marave_es.ts
@@ -18,37 +18,65 @@
         <translation>Esc</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="145"/>
+=======
         <location filename="../Ui_prefs.py" line="120"/>
+>>>>>>> .r325
         <source>&amp;Theme:</source>
         <translation>&amp;Tema:</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="148"/>
+=======
         <location filename="../Ui_prefs.py" line="123"/>
+>>>>>>> .r325
         <source>&amp;Spell Checking:</source>
         <translation>&amp;Corrector Ortográfico:</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="151"/>
+=======
         <location filename="../Ui_prefs.py" line="126"/>
+>>>>>>> .r325
         <source>&amp;Buttons:</source>
         <translation>&amp;Botones:</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="152"/>
+=======
         <location filename="../Ui_prefs.py" line="127"/>
+>>>>>>> .r325
         <source>Icons</source>
         <translation>Iconos</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="153"/>
+=======
         <location filename="../Ui_prefs.py" line="128"/>
+>>>>>>> .r325
         <source>Text</source>
         <translation>Texto</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="154"/>
+=======
         <location filename="../Ui_prefs.py" line="129"/>
+>>>>>>> .r325
         <source>Text + Icons</source>
         <translation>Texto + Iconos</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="155"/>
+=======
         <location filename="../Ui_prefs.py" line="130"/>
+>>>>>>> .r325
         <source>&amp;Editor Opacity:</source>
         <translation>Opacidad del &amp;editor:</translation>
     </message>
@@ -88,40 +116,78 @@
         <translation>Reemplazar &amp;Todo</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="156"/>
+=======
         <location filename="../Ui_prefs.py" line="131"/>
+>>>>>>> .r325
         <source>Auto save (minutes):</source>
         <translation>Guardar automáticamente (minutos):</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="147"/>
+=======
         <location filename="../Ui_prefs.py" line="122"/>
+>>>>>>> .r325
         <source>St&amp;yle:</source>
         <translation>E&amp;stilo:</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="149"/>
+=======
         <location filename="../Ui_prefs.py" line="124"/>
+>>>>>>> .r325
         <source>Syntax Highlight:</source>
         <translation>Resaltado de sintaxis:</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../Ui_prefs.py" line="150"/>
+=======
         <location filename="../Ui_prefs.py" line="125"/>
+>>>>>>> .r325
         <source>Color Scheme:</source>
         <translation>Esquema de colores:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="157"/>
+        <source>Settings</source>
+        <translation>Opciones</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="158"/>
+        <source>Plugins</source>
+        <translation>Plugins</translation>
     </message>
 </context>
 <context>
     <name>MainWidget</name>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="599"/>
+=======
         <location filename="../main.py" line="572"/>
+>>>>>>> .r325
         <source>Sound support is not available, disabling sound</source>
         <translation>Soporte para sonido no disponible, inhabilitando sonidos</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="609"/>
+=======
         <location filename="../main.py" line="582"/>
+>>>>>>> .r325
         <source>Document: %s</source>
         <translation>Documento: %s</translation>
     </message>
     <message numerus="yes">
+<<<<<<< .mine
+        <location filename="../main.py" line="610"/>
+=======
         <location filename="../main.py" line="583"/>
+>>>>>>> .r325
         <source>%n words</source>
         <translation>
             <numerusform>%n palabra</numerusform>
@@ -129,7 +195,11 @@
         </translation>
     </message>
     <message numerus="yes">
+<<<<<<< .mine
+        <location filename="../main.py" line="611"/>
+=======
         <location filename="../main.py" line="584"/>
+>>>>>>> .r325
         <source>%n lines</source>
         <translation>
             <numerusform>%n línea</numerusform>
@@ -137,7 +207,11 @@
         </translation>
     </message>
     <message numerus="yes">
+<<<<<<< .mine
+        <location filename="../main.py" line="612"/>
+=======
         <location filename="../main.py" line="585"/>
+>>>>>>> .r325
         <source>%n characters</source>
         <translation>
             <numerusform>%n caracter</numerusform>
@@ -145,220 +219,384 @@
         </translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="898"/>
+=======
         <location filename="../main.py" line="871"/>
+>>>>>>> .r325
         <source>Close Document - Marave</source>
         <translation>Cerrar Documento - Marave</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="898"/>
+=======
         <location filename="../main.py" line="871"/>
+>>>>>>> .r325
         <source>The document &quot;%s&quot; has been modified.</source>
         <translation>El documento &quot;%s&quot; ha sido modificado.</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="898"/>
+=======
         <location filename="../main.py" line="871"/>
+>>>>>>> .r325
         <source>UNNAMED</source>
         <translation>SIN NOMBRE</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="898"/>
+=======
         <location filename="../main.py" line="871"/>
+>>>>>>> .r325
         <source>
 Do you want to save your changes or discard them?</source>
         <translation>¿Desea guardar sus cambios o descartarlos?</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="421"/>
+=======
         <location filename="../main.py" line="394"/>
+>>>>>>> .r325
         <source>Font</source>
         <translation>TIpografía
 </translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="422"/>
+=======
         <location filename="../main.py" line="395"/>
+>>>>>>> .r325
         <source>Size</source>
         <translation>Tamaño</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="423"/>
+=======
         <location filename="../main.py" line="396"/>
+>>>>>>> .r325
         <source>File</source>
         <translation>Archivo</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="424"/>
+=======
         <location filename="../main.py" line="397"/>
+>>>>>>> .r325
         <source>Bg</source>
         <translation>Fondo</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="426"/>
+=======
         <location filename="../main.py" line="399"/>
+>>>>>>> .r325
         <source>Click</source>
         <translation>Click</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="427"/>
+=======
         <location filename="../main.py" line="400"/>
+>>>>>>> .r325
         <source>Music</source>
         <translation>Música</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="428"/>
+=======
         <location filename="../main.py" line="401"/>
+>>>>>>> .r325
         <source>Options</source>
         <translation>Opciones</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="430"/>
+=======
         <location filename="../main.py" line="403"/>
+>>>>>>> .r325
         <source>Quit</source>
         <translation>Salir</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="470"/>
+=======
         <location filename="../main.py" line="443"/>
+>>>>>>> .r325
         <source>Color</source>
         <translation>Color</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="451"/>
+=======
         <location filename="../main.py" line="424"/>
+>>>>>>> .r325
         <source>Smaller</source>
         <translation>Menor</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="452"/>
+=======
         <location filename="../main.py" line="425"/>
+>>>>>>> .r325
         <source>Default</source>
         <translation>Normal</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="453"/>
+=======
         <location filename="../main.py" line="426"/>
+>>>>>>> .r325
         <source>Larger</source>
         <translation>Mayor</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="460"/>
+=======
         <location filename="../main.py" line="433"/>
+>>>>>>> .r325
         <source>Open</source>
         <translation>Abrir</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="461"/>
+=======
         <location filename="../main.py" line="434"/>
+>>>>>>> .r325
         <source>Save</source>
         <translation>Guardar</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="462"/>
+=======
         <location filename="../main.py" line="435"/>
+>>>>>>> .r325
         <source>Save As</source>
         <translation>Guardar Como</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="485"/>
+=======
         <location filename="../main.py" line="458"/>
+>>>>>>> .r325
         <source>Previous</source>
         <translation>Anterior</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="486"/>
+=======
         <location filename="../main.py" line="459"/>
+>>>>>>> .r325
         <source>Next</source>
         <translation>Siguiente</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="487"/>
+=======
         <location filename="../main.py" line="460"/>
+>>>>>>> .r325
         <source>None</source>
         <translation>Ninguna</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="658"/>
+=======
         <location filename="../main.py" line="631"/>
+>>>>>>> .r325
         <source>Setting opacity to: %s%%</source>
         <translation>Opacidad: %s%%</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="1017"/>
+=======
         <location filename="../main.py" line="990"/>
+>>>>>>> .r325
         <source>Switching click to: %s</source>
         <translation>Cambiando click a: %s</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="1045"/>
+=======
         <location filename="../main.py" line="1018"/>
+>>>>>>> .r325
         <source>Disabling click</source>
         <translation>Inhabilitando click</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="1051"/>
+=======
         <location filename="../main.py" line="1024"/>
+>>>>>>> .r325
         <source>Listening to: %s</source>
         <translation>Escuchando: %s</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="1084"/>
+=======
         <location filename="../main.py" line="1057"/>
+>>>>>>> .r325
         <source>Disabling music</source>
         <translation>Inhabilitando música</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="1143"/>
+=======
         <location filename="../main.py" line="1116"/>
+>>>>>>> .r325
         <source>Setting background to: %s</source>
         <translation>Cambiando fondo a: %s</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="685"/>
+=======
         <location filename="../main.py" line="658"/>
+>>>>>>> .r325
         <source>Changing to style %s requires restarting Marave</source>
         <translation>Cambiar al estilo %s requiere reiniciar Marave</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="367"/>
+=======
         <location filename="../main.py" line="340"/>
+>>>>>>> .r325
         <source>Ctrl+F</source>
         <comment>Find</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="369"/>
+=======
         <location filename="../main.py" line="342"/>
+>>>>>>> .r325
         <source>Ctrl+R</source>
         <comment>Find and Replace</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="373"/>
+=======
         <location filename="../main.py" line="346"/>
+>>>>>>> .r325
         <source>Ctrl+T</source>
         <comment>Taj Mode</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="381"/>
+=======
         <location filename="../main.py" line="354"/>
+>>>>>>> .r325
         <source>Ctrl+O</source>
         <comment>Open File</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="383"/>
+=======
         <location filename="../main.py" line="356"/>
+>>>>>>> .r325
         <source>Ctrl+S</source>
         <comment>Save File</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="385"/>
+=======
         <location filename="../main.py" line="358"/>
+>>>>>>> .r325
         <source>Shift+Ctrl+S</source>
         <comment>Save File As</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="387"/>
+=======
         <location filename="../main.py" line="360"/>
+>>>>>>> .r325
         <source>Ctrl+N</source>
         <comment>New File</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="389"/>
+=======
         <location filename="../main.py" line="362"/>
+>>>>>>> .r325
         <source>Ctrl+Q</source>
         <comment>Quit</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="392"/>
+=======
         <location filename="../main.py" line="365"/>
+>>>>>>> .r325
         <source>Shift+Ctrl+P</source>
         <comment>Show Preferences</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="396"/>
+=======
         <location filename="../main.py" line="369"/>
+>>>>>>> .r325
         <source>Ctrl+I</source>
         <comment>Show Document Info</comment>
         <translation></translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="400"/>
+=======
         <location filename="../main.py" line="373"/>
+>>>>>>> .r325
         <source>F1</source>
         <comment>Help</comment>
         <translation></translation>
     </message>
     <message numerus="yes">
+<<<<<<< .mine
+        <location filename="../main.py" line="583"/>
+=======
         <location filename="../main.py" line="556"/>
+>>>>>>> .r325
         <source>Saving every %n minutes</source>
         <translation>
             <numerusform>Guardando cada %n minuto</numerusform>
@@ -366,12 +604,31 @@ Do you want to save your changes or discard them?</source>
         </translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="587"/>
+=======
         <location filename="../main.py" line="560"/>
+>>>>>>> .r325
         <source>Disabled automatic saving</source>
         <translation>Guardado automático inhabilitado</translation>
     </message>
 </context>
 <context>
+<<<<<<< .mine
+    <name>PrefsWidget</name>
+    <message>
+        <location filename="../main.py" line="148"/>
+        <source>None</source>
+        <translation>Ninguna</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="158"/>
+        <source>Current</source>
+        <translation>Actual</translation>
+    </message>
+</context>
+<context>
+=======
     <name>PrefsWidget</name>
     <message>
         <location filename="../main.py" line="131"/>
@@ -380,6 +637,7 @@ Do you want to save your changes or discard them?</source>
     </message>
 </context>
 <context>
+>>>>>>> .r325
     <name>SpellTextEdit</name>
     <message>
         <location filename="../spelltextedit.py" line="137"/>
@@ -395,12 +653,20 @@ Do you want to save your changes or discard them?</source>
 <context>
     <name>app</name>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="1435"/>
+=======
         <location filename="../main.py" line="1396"/>
+>>>>>>> .r325
         <source>FOCUS!</source>
         <translation>¡Enfócate!</translation>
     </message>
     <message>
+<<<<<<< .mine
+        <location filename="../main.py" line="1435"/>
+=======
         <location filename="../main.py" line="1396"/>
+>>>>>>> .r325
         <source>Marave only opens one document at a time.
 The whole idea is focusing!
 So, this is the first one you asked for.</source>

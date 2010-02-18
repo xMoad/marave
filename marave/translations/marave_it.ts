13543c0b9abb096d22fb6513b580eb718249d793
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-18 19:55:33 +0000

    New translation to italian by Pierpaolo Da Fieno

---
+++ marave/translations/marave_it.ts
@@ -0,0 +1,427 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="it_IT">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../Ui_searchwidget.py" line="59"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchwidget.py" line="60"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchwidget.py" line="61"/>
+        <source>Esc</source>
+        <translation>Esc</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="146"/>
+        <source>&amp;Theme:</source>
+        <translation>&amp;Tema:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="149"/>
+        <source>&amp;Spell Checking:</source>
+        <translation>&amp;Correttore Ortografico:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="152"/>
+        <source>&amp;Buttons:</source>
+        <translation>&amp;Pulsanti:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="153"/>
+        <source>Icons</source>
+        <translation>Icone</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="154"/>
+        <source>Text</source>
+        <translation>Testo</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="155"/>
+        <source>Text + Icons</source>
+        <translation>Testo + Icone</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="156"/>
+        <source>&amp;Editor Opacity:</source>
+        <translation>Opacitá dell&apos;&amp;editor:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchwidget.py" line="62"/>
+        <source>&amp;Find:</source>
+        <translation>&amp;Trova:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchwidget.py" line="63"/>
+        <source>&amp;Previous</source>
+        <translation>&amp;Precedente</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchwidget.py" line="64"/>
+        <source>&amp;Next</source>
+        <translation>&amp;Successivo</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchwidget.py" line="65"/>
+        <source>&amp;Match Case</source>
+        <translation>&amp;Maiuscole/minuscole</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchreplacewidget.py" line="94"/>
+        <source>Rep&amp;lace:</source>
+        <translation>&amp;Sostituisci:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchreplacewidget.py" line="95"/>
+        <source>&amp;Replace</source>
+        <translation>&amp;Sostituisci</translation>
+    </message>
+    <message>
+        <location filename="../Ui_searchreplacewidget.py" line="96"/>
+        <source>Replace &amp;All</source>
+        <translation>Sostituisci &amp;Tutto</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="157"/>
+        <source>Auto save (minutes):</source>
+        <translation>Salva automaticamente (minuti):</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="148"/>
+        <source>St&amp;yle:</source>
+        <translation>St&amp;ile:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="150"/>
+        <source>Syntax Highlight:</source>
+        <translation>Evidenziatore Sintassi:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="151"/>
+        <source>Color Scheme:</source>
+        <translation>Schema Colore:</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="158"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../Ui_prefs.py" line="159"/>
+        <source>Plugins</source>
+        <translation>Plugins</translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="../main.py" line="624"/>
+        <source>Sound support is not available, disabling sound</source>
+        <translation>Supporto sonoro non disponibile, suono disabilitato</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="634"/>
+        <source>Document: %s</source>
+        <translation>Documento: %s</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../main.py" line="635"/>
+        <source>%n words</source>
+        <translation>
+            <numerusform>%n parola</numerusform>
+            <numerusform>%n parole</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../main.py" line="636"/>
+        <source>%n lines</source>
+        <translation>
+            <numerusform>%n linea</numerusform>
+            <numerusform>%n linee</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../main.py" line="637"/>
+        <source>%n characters</source>
+        <translation>
+            <numerusform>%n carattere</numerusform>
+            <numerusform>%n caratteri</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="923"/>
+        <source>Close Document - Marave</source>
+        <translation>Chiudi Documento - Marave</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="923"/>
+        <source>The document &quot;%s&quot; has been modified.</source>
+        <translation>Ill documento &quot;%s&quot; é stato modificato.</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="923"/>
+        <source>UNNAMED</source>
+        <translation>SENZA NOME</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="923"/>
+        <source>
+Do you want to save your changes or discard them?</source>
+        <translation>Vuoi salvare le modifiche o scartarle?</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="446"/>
+        <source>Font</source>
+        <translation>Tipo di Carattere
+</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="447"/>
+        <source>Size</source>
+        <translation>Dimensione</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="448"/>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="449"/>
+        <source>Bg</source>
+        <translation>Sfondo</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="451"/>
+        <source>Click</source>
+        <translation>Click</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="452"/>
+        <source>Music</source>
+        <translation>Musica</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="453"/>
+        <source>Options</source>
+        <translation>Opzioni</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="455"/>
+        <source>Quit</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="495"/>
+        <source>Color</source>
+        <translation>Colore</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="476"/>
+        <source>Smaller</source>
+        <translation>Piú piccolo</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="477"/>
+        <source>Default</source>
+        <translation>Normale</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="478"/>
+        <source>Larger</source>
+        <translation>Piú grande</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="485"/>
+        <source>Open</source>
+        <translation>Apri</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="486"/>
+        <source>Save</source>
+        <translation>Salva</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="487"/>
+        <source>Save As</source>
+        <translation>Salva con nome</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="510"/>
+        <source>Previous</source>
+        <translation>Precedente</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="511"/>
+        <source>Next</source>
+        <translation>Successivo</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="512"/>
+        <source>None</source>
+        <translation>Nessuno</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="683"/>
+        <source>Setting opacity to: %s%%</source>
+        <translation>Opacitá: %s%%</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="1042"/>
+        <source>Switching click to: %s</source>
+        <translation>Click selezionato: %s</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="1070"/>
+        <source>Disabling click</source>
+        <translation>Click disabilitato</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="1076"/>
+        <source>Listening to: %s</source>
+        <translation>Stai ascoltando: %s</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="1109"/>
+        <source>Disabling music</source>
+        <translation>Musica disabilitata</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="1168"/>
+        <source>Setting background to: %s</source>
+        <translation>Sfondo selezionato: %s</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="710"/>
+        <source>Changing to style %s requires restarting Marave</source>
+        <translation>Cambiare lo stile %s richiede il riavvio di Marave</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="392"/>
+        <source>Ctrl+F</source>
+        <comment>Find</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="394"/>
+        <source>Ctrl+R</source>
+        <comment>Find and Replace</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="398"/>
+        <source>Ctrl+T</source>
+        <comment>Taj Mode</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="406"/>
+        <source>Ctrl+O</source>
+        <comment>Open File</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="408"/>
+        <source>Ctrl+S</source>
+        <comment>Save File</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="410"/>
+        <source>Shift+Ctrl+S</source>
+        <comment>Save File As</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="412"/>
+        <source>Ctrl+N</source>
+        <comment>New File</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="414"/>
+        <source>Ctrl+Q</source>
+        <comment>Quit</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="417"/>
+        <source>Shift+Ctrl+P</source>
+        <comment>Show Preferences</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="421"/>
+        <source>Ctrl+I</source>
+        <comment>Show Document Info</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="425"/>
+        <source>F1</source>
+        <comment>Help</comment>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../main.py" line="608"/>
+        <source>Saving every %n minutes</source>
+        <translation>
+            <numerusform>Salvando ogni %n minuto</numerusform>
+            <numerusform>Salvando ogni %n minuti</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="612"/>
+        <source>Disabled automatic saving</source>
+        <translation>Salvataggio automatico disabiltato</translation>
+    </message>
+</context>
+<context>
+    <name>PrefsWidget</name>
+    <message>
+        <location filename="../main.py" line="173"/>
+        <source>None</source>
+        <translation>Nessuno</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="183"/>
+        <source>Current</source>
+        <translation>Corrente</translation>
+    </message>
+</context>
+<context>
+    <name>SpellTextEdit</name>
+    <message>
+        <location filename="../spelltextedit.py" line="137"/>
+        <source>Document saved</source>
+        <translation>Documento salvato</translation>
+    </message>
+    <message>
+        <location filename="../spelltextedit.py" line="100"/>
+        <source>Spelling Suggestions</source>
+        <translation>Correzioni suggerite</translation>
+    </message>
+</context>
+<context>
+    <name>app</name>
+    <message>
+        <location filename="../main.py" line="1460"/>
+        <source>FOCUS!</source>
+        <translation>Concentrati!</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="1460"/>
+        <source>Marave only opens one document at a time.
+The whole idea is focusing!
+So, this is the first one you asked for.</source>
+        <translation>Marave apre un solo documento per volta
+L&apos;idea é di concentrarsi!
+Perció questo é il primo che hai chiesto.</translation>
+    </message>
+</context>
+</TS>

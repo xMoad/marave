c46fa28f6afbd6838a712ab9c529dd38ad4566cd
roberto.alsina <roberto.alsina@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-21 21:34:53 +0000

    Minor doc change

--- branches/0.6/marave/Ui_searchreplacewidget.py
+++ branches/0.6/marave/Ui_searchreplacewidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'searchreplwidget.ui'
 #
-# Created: Mon Feb 15 15:52:50 2010
+# Created: Sun Feb 21 18:35:41 2010
 #      by: PyQt4 UI code generator 4.7
 #
 # WARNING! All changes made in this file will be lost!

8fab6b7eed41d8da17caf6669080d53b04e3a930
bdgraue <bdgraue@57fa06e0-0c1b-11df-9e2b-972ea447d3bf>
2010-02-21 18:12:01 +0000

    updated german translation

--- trunk/marave/translations/marave_de.ts
+++ trunk/marave/translations/marave_de.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="de_DE">
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
 <context>
     <name>Form</name>
     <message>
@@ -383,7 +384,7 @@ Möchten Sie die Änderungen speichern oder verwerfen?</translation>
     <message>
         <location filename="../main.py" line="432"/>
         <source>Esc</source>
-        <translation type="unfinished">Esc</translation>
+        <translation>Esc</translation>
     </message>
 </context>
 <context>
